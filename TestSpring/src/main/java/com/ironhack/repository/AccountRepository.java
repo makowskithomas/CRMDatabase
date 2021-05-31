58d3b94fd782ce268ea96469abab4fcc3a8eb852
Sebastian Siegling <s.sgl@t-online.de>
2021-05-31 19:14:55 +0200

    Added Yes or Now Function for a new Account in convert lead

--- TestSpring/src/main/java/com/ironhack/repository/AccountRepository.java
+++ TestSpring/src/main/java/com/ironhack/repository/AccountRepository.java
@@ -1,13 +1,14 @@
 package com.ironhack.repository;
 
 import com.ironhack.crm.Account;
-import com.ironhack.crm.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 
     public interface AccountRepository extends JpaRepository<Account, Integer> {
 
-
+//    Account findById(Integer id);
 }

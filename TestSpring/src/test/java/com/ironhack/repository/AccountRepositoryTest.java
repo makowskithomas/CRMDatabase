de8fe757eb4ddacbb59206ff79fe54608ac7b115
Patrick77.7 <patric.ius@live.de>
2021-06-01 19:51:55 +0200

    -Remove opportunity from contact class + further code in generatetestdata

--- TestSpring/src/test/java/com/ironhack/repository/AccountRepositoryTest.java
+++ TestSpring/src/test/java/com/ironhack/repository/AccountRepositoryTest.java
@@ -2,10 +2,7 @@ package com.ironhack.repository;
 
 import com.ironhack.crm.Account;
 import com.ironhack.crm.Industry;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,15 +21,18 @@ class AccountRepositoryTest {
 
     @BeforeEach
     public void setUp(){
-        accountRepository.deleteAll();
+        //accountRepository.deleteAll();
         accountRepository.save(new Account(Industry.MEDICAL,3000,"testBerlin","testGermany"));
         accountRepository.save(new Account(Industry.ECOMMERCE,3000,"testLondon","testUK"));
+        accountRepository.save(new Account(Industry.MANUFACTURING,4000,"testParis","testFrance"));
+        accountRepository.save(new Account(Industry.PRODUCE,1000,"testLondon","testUK"));
+        accountRepository.save(new Account(Industry.OTHER,5000,"testLondon","testUK"));
     }
 
     @Test
     public void testFindAll(){
         List<Account> accountList = accountRepository.findAll();
-        assertEquals(2,accountList.size());
+        assertEquals(5,accountList.size());
     }
 
     @Test
@@ -42,5 +42,36 @@ class AccountRepositoryTest {
         assertEquals("testBerlin",account1.getCity());
     }
 
+    @Test
+    void getMeanEmployeeCount() {
+        Double test= (double) accountRepository.getMeanEmployeeCount();
+        assertEquals(3200,test);
+    }
 
+
+    @Test
+    void getMedianEmployeeCount() {
+        Integer[] accountList= accountRepository.getListForMedianEmployeeCount();
+        Double median;
+        Integer h1;
+        Integer h2;
+        if(accountList.length % 2 == 0){
+            h1= accountList[(accountList.length/2-1)];
+            h2= accountList[(accountList.length/2)];
+            median= (double) (h1+h2)/2;
+        } else {
+            median= (double) accountList[(int) Math.floor(accountList.length/2)];
+        }
+        assertEquals(3000,median);
+    }
+
+    @Test
+    void getMaxEmployeeCount() {
+        assertEquals(5000,accountRepository.getMaxEmployeeCount());
+    }
+
+    @Test
+    void getMinEmployeeCount() {
+        assertEquals(1000,accountRepository.getMinEmployeeCount());
+    }
 }

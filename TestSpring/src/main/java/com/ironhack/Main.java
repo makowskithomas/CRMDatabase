9a99f1ccba70294374e8d6a8347087104899bac9
Tim Acosta <timacosta06@gmail.com>
2021-06-01 11:40:06 +0100

    EXAMPLE of how it can work and repos be initialized, only working for leads

--- TestSpring/src/main/java/com/ironhack/Main.java
+++ TestSpring/src/main/java/com/ironhack/Main.java
@@ -2,15 +2,38 @@ package com.ironhack;
 
 
 import com.ironhack.cli.CLI;
+import com.ironhack.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    LeadRepository leadRepository;
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    OpportunityRepository opportunityRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    SalesRepRepository salesRepRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        CLI.start();
+
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        CLI.initRepository(leadRepository, contactRepository, opportunityRepository, accountRepository, salesRepRepository);
+        CLI.start();
+    }
 }
